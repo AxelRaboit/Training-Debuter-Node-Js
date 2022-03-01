cacae101ba2a1742ec8bd577fed633893a77163b
Axel Raboit <axel.raboit@gmail.com>
2022-03-01 11:17:46 +0100

    add crud in node js

--- course_nodejs_ejs/app.js
+++ course_nodejs_ejs/app.js
@@ -1,19 +1,99 @@
 const express = require('express');
 const app = express();
+const mySql = require('mysql');
+const myConnection = require('express-myconnection');
+const { redirect } = require('express/lib/response');
+
+const optionBd = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: '8889',
+    database: 'notes_training_nodejs'
+};
+
+//Extraction des datas du formulaire
+app.use(express.urlencoded({extended: false}))
+
+
+//Definition du middleware pour connexion avec la bd
+app.use(myConnection(mySql, optionBd, 'pool'));
+
 
 //Definition du moteur d'affichage
 app.set('view engine', 'ejs')
 app.set('views', 'IHM')
 
+//Method to read data
 app.get('/', (req, res) => {
-    const connectedHour = Date().toString();
-    const notes = [
-        {title: 'Création de contenu', description: 'Tourner un épisode pour youtube'},
-        {title: 'Education Physique', description: 'Course à pied'},
-        {title: 'Dessin numérique', description: 'Création d\'une image sur photoshop'},
-        {title: 'Magasin', description: 'Faire les courses'}
-    ]
-    res.status(200).render('index', {connectedHour, notes});
+
+    req.getConnection((err, connection) => {
+        if(err) {
+            console.log(err)
+        } else {
+            connection.query('SELECT * FROM notes', [], (err, result) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    res.status(200).render('index', { result });
+                }
+            })
+        }
+    })
+})
+
+//Method to post data
+app.post('/notes', (req, res) => {
+    console.log(req.body)
+    let id = req.body.id === '' ? null : req.body.id;
+    let title = req.body.title;
+    let description = req.body.description;
+
+    let reqSql = id === null 
+        ? 'INSERT INTO notes(id, title, description) VALUES(?, ?, ?)'
+        : 'UPDATE notes SET title = ?, description = ? WHERE id = ?'
+    ;
+
+    let data = id === null 
+        ? [null, title, description]
+        : [title, description, id]
+    ;
+
+    req.getConnection((err, connection) => {
+        if(err) {
+            console.log(err)
+        } else {
+            connection.query(
+            reqSql,
+            data,
+            (err, result) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    res.status(300).redirect('/');
+                }
+            })
+        }
+    })
+})
+
+//Method to delete data
+app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    req.getConnection((err, connection) => {
+        if(err) {
+            console.log(err)
+        } else {
+            connection.query('DELETE FROM notes WHERE id = ?', [id], (err, result) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    res.status(200).json({routeRacine: '/'})
+                }
+            })
+        }
+    })
+
 })
 
 app.get('/about', (req, res) => {
