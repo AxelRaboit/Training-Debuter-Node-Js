6e6fc505d95e619ed78101bd7d83229afef6cdc9
Axel Raboit <axel.raboit@gmail.com>
2022-03-01 12:06:05 +0100

    deport routes in a file

--- course_nodejs_ejs/app.js
+++ course_nodejs_ejs/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mySql = require('mysql');
 const myConnection = require('express-myconnection');
-const { redirect } = require('express/lib/response');
+const notesRoute = require('./routes/notesRoute');
 
 const optionBd = {
     host: 'localhost',
@@ -24,77 +24,8 @@ app.use(myConnection(mySql, optionBd, 'pool'));
 app.set('view engine', 'ejs')
 app.set('views', 'IHM')
 
-//Method to read data
-app.get('/', (req, res) => {
-
-    req.getConnection((err, connection) => {
-        if(err) {
-            console.log(err)
-        } else {
-            connection.query('SELECT * FROM notes', [], (err, result) => {
-                if(err) {
-                    console.log(err)
-                } else {
-                    res.status(200).render('index', { result });
-                }
-            })
-        }
-    })
-})
-
-//Method to post data
-app.post('/notes', (req, res) => {
-    console.log(req.body)
-    let id = req.body.id === '' ? null : req.body.id;
-    let title = req.body.title;
-    let description = req.body.description;
-
-    let reqSql = id === null 
-        ? 'INSERT INTO notes(id, title, description) VALUES(?, ?, ?)'
-        : 'UPDATE notes SET title = ?, description = ? WHERE id = ?'
-    ;
-
-    let data = id === null 
-        ? [null, title, description]
-        : [title, description, id]
-    ;
-
-    req.getConnection((err, connection) => {
-        if(err) {
-            console.log(err)
-        } else {
-            connection.query(
-            reqSql,
-            data,
-            (err, result) => {
-                if(err) {
-                    console.log(err)
-                } else {
-                    res.status(300).redirect('/');
-                }
-            })
-        }
-    })
-})
-
-//Method to delete data
-app.delete('/notes/:id', (req, res) => {
-    const id = req.params.id;
-    req.getConnection((err, connection) => {
-        if(err) {
-            console.log(err)
-        } else {
-            connection.query('DELETE FROM notes WHERE id = ?', [id], (err, result) => {
-                if(err) {
-                    console.log(err)
-                } else {
-                    res.status(200).json({routeRacine: '/'})
-                }
-            })
-        }
-    })
-
-})
+//Definition des routes pour notes
+app.use(notesRoute)
 
 app.get('/about', (req, res) => {
     res.status(200).render('about')
---
+++ course_nodejs_ejs/routes/notesRoute.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router()
+
+//Method to read data
+router.get('/', (req, res) => {
+
+    req.getConnection((err, connection) => {
+        if(err) {
+            console.log(err)
+        } else {
+            connection.query('SELECT * FROM notes', [], (err, result) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    res.status(200).render('index', { result });
+                }
+            })
+        }
+    })
+})
+
+//Method to post data
+router.post('/notes', (req, res) => {
+    console.log(req.body)
+    let id = req.body.id === '' ? null : req.body.id;
+    let title = req.body.title;
+    let description = req.body.description;
+
+    let reqSql = id === null 
+        ? 'INSERT INTO notes(id, title, description) VALUES(?, ?, ?)'
+        : 'UPDATE notes SET title = ?, description = ? WHERE id = ?'
+    ;
+
+    let data = id === null 
+        ? [null, title, description]
+        : [title, description, id]
+    ;
+
+    req.getConnection((err, connection) => {
+        if(err) {
+            console.log(err)
+        } else {
+            connection.query(
+            reqSql,
+            data,
+            (err, result) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    res.status(300).redirect('/');
+                }
+            })
+        }
+    })
+})
+
+//Method to delete data
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    req.getConnection((err, connection) => {
+        if(err) {
+            console.log(err)
+        } else {
+            connection.query('DELETE FROM notes WHERE id = ?', [id], (err, result) => {
+                if(err) {
+                    console.log(err)
+                } else {
+                    res.status(200).json({routeRacine: '/'})
+                }
+            })
+        }
+    })
+
+})
+
+module.exports = router;
